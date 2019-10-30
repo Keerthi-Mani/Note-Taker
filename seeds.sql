7f6c4c97e664d1aacff46cfd7e6f69e4b16ed41e
Keerthi Mani <carkeane.getz@gmail.com>
2019-10-29 22:47:14 -0400

    Html codes are ready

---
+++ seeds.sql
@@ -0,0 +1,13 @@
+USE notetaker_db;
+
+INSERT INTO notes (title, body)
+VALUES
+  ("Note Taker App", "Complete this homework by Tuesday, August 6, 2019 11:59 PM"),
+  ("READ THE DOCS", "Read documentation about... Node, Express, MySQL, etc."),
+  ("Mark myself present in Bootcampspot", "Remember to mark myself present every Monday-Friday at 10am"),
+  ("Commit more", "Get those commits up. It shows the process and progress.")
+  ("Minimum Requirements", "Attempt to complete homework assignment as described in instructions."),
+  ("Submission on BCS", "Please submit both the deployed Heroku link to your homework AND the link to the Github Repository!"),
+  ("Hosting on Heroku", "Now that we have a backend to our applications, we use Heroku for hosting."),
+  ("Create a README.md", "After completing the homework please add the piece to your portfolio."),
+  
